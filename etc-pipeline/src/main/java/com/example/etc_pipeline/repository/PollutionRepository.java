661962638a08aa8a2607c565f93ace49786feb7b
Arpanel Franklin <arpanelfranklin@Arpanels-MacBook-Air.local>
2026-06-02 20:07:15 +0530

    minor update and Docker files for backend

--- etc-pipeline/src/main/java/com/example/etc_pipeline/repository/PollutionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.etc_pipeline.repository;
-
-import com.example.etc_pipeline.entity.PollutionData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PollutionRepository extends MongoRepository<PollutionData,String> {
-}
--- etc-pipeline/src/main/java/com/example/etc_pipeline/scheduler/EtlScheduler.java
+++
@@ -1,4 +0,0 @@
-package com.example.etc_pipeline.scheduler;
-
-public class EtlScheduler {
-}
